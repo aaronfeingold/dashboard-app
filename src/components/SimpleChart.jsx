1072a702310e012de2b7ccb6fa66643e9d9559e8
aaron.feingold <ajfeingold88@gmail.com>
2025-05-19 11:26:18 -0500

    a fun bar chart hardcoded

---
+++ src/components/SimpleChart.jsx
@@ -0,0 +1,31 @@
+const SimpleChart = () => {
+  // This would be a placeholder for a real chart library like Chart.js or Recharts
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div className="h-full flex items-end gap-2 pt-4">
+        {[35, 60, 25, 75, 45, 65, 35, 85, 55, 40, 70].map((value, i) => (
+          <div
+            key={i}
+            className="bg-indigo-500 w-full rounded-t"
+            style={{ height: `${value}%` }}
+          ></div>
+        ))}
+      </div>
+      <div className="h-6 flex justify-between text-xs text-gray-500 mt-2">
+        <span>Jan</span>
+        <span>Feb</span>
+        <span>Mar</span>
+        <span>Apr</span>
+        <span>May</span>
+        <span>Jun</span>
+        <span>Jul</span>
+        <span>Aug</span>
+        <span>Sep</span>
+        <span>Oct</span>
+        <span>Nov</span>
+      </div>
+    </div>
+  );
+};
+
+export default SimpleChart;
